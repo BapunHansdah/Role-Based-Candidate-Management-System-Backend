c80e4683a3d6d361a928926e324b8d4130b27da4
bapunhansdah <bapunhansdah777@gmail.com>
2025-01-05 22:51:44 +0530

    chang

--- index.js
+++ index.js
@@ -23,6 +23,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://role-based-candidate-management-system.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
+app.use('/api/test', (req, res) => {
+  res.json({ message: 'API is working' });
+})
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
