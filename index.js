a35a4681d6cb5046577d08cc4cf50c62dab9db04
bapunhansdah <bapunhansdah777@gmail.com>
2025-01-05 22:45:38 +0530

    url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'http://localhost:5173', // Replace with your frontend URL
+    origin: 'https://role-based-candidate-management-system.vercel.app', // Replace with your frontend URL
     credentials: true,
   })
 );
