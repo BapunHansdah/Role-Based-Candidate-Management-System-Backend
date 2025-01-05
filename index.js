44b0c2c5bd6fc4463fc33c8bf74c7d9cfbcd8737
bapunhansdah <bapunhansdah777@gmail.com>
2025-01-05 23:06:38 +0530

    change

--- index.js
+++ index.js
@@ -18,17 +18,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: 'https://role-based-candidate-management-system.vercel.app', // Replace with your frontend URL
-    credentials: true,
+    origin: '*', 
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://role-based-candidate-management-system.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-})
 
 app.use('/api/test', (req, res) => {
   res.json({ message: 'API is working' });
